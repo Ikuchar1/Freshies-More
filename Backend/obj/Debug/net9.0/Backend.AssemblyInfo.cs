5ecac9de54b26c8fe16b2273f3efd0c023927d3b
Ikuchar1 <ian1kuchar@gmail.com>
2025-03-22 16:45:16 -0500

    env working

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e594b54ad6b4fdf874b0733d7cf4a69e861880e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9de2ecafe14698459b7c9a2e5f5a051bdcb60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
